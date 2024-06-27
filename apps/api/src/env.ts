8127ed49b99de69f29089b6583a7b61dd8752159
b12k <bogdan.kolesnyk@gmail.com>
2024-06-27 10:24:57 +0200

    wip

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,14 +1,14 @@
 import z from 'zod';
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['development', 'production']),
-    PORT: z.preprocess(Number, z.number()),
-    SEARCH_URL: z.string().url(),
+  NODE_ENV: z.enum(['development', 'production']),
+  PORT: z.preprocess(Number, z.number()),
+  SEARCH_URL: z.string().url(),
 });
 
 const parsedEnvSchema = envSchema.parse(process.env);
 
-export type Env = z.infer<typeof envSchema>
+export type Env = z.infer<typeof envSchema>;
 export const env = {
   ...parsedEnvSchema,
   IS_PROD: parsedEnvSchema.NODE_ENV === 'production',
